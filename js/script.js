3b01284f43ad07385932db8f73e8d2fd6d272aa7
Maxi <maxi21498@gmail.com>
2022-11-04 16:43:47 -0300

    Adaptacion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { generarCard } from "./dinamic-template.js";
+import { generarCard, generarShow } from "./dinamic-template.js";
 
 function createLocalStorage() {
   //Genera un objeto que luego es agregado al local storage
@@ -89,39 +89,16 @@ function createLocalStorage() {
 localStorage.clear();
 createLocalStorage();
 
-// export function generarCard(option) {
-//   const $templateCard = document.getElementById("template-card").content,
-//     $cards = document.querySelector(".cards"),
-//     $fragment = document.createDocumentFragment();
-//   //Genera tarjetas dinamicas en los html donde esta funcion sea llamada,dependiendo del option que le pase toma los datos del LocalSotorage
-//   let prueba = localStorage.getItem(option);
-//   prueba = JSON.parse(prueba);
-//   prueba.forEach((element, index) => {
-//     let { img1, img2, img3, alt, description, price } = element;
-//     $templateCard.querySelector("div").setAttribute("data-id", index);
-//     $templateCard.querySelector("img").setAttribute("src", img1);
-//     $templateCard.querySelector("img").setAttribute("alt", alt);
-//     $templateCard.querySelectorAll("p")[0].textContent = description;
-//     $templateCard.querySelectorAll("p")[1].textContent = `$${price}`;
-//     const $clon = document.importNode($templateCard, true);
-//     $fragment.appendChild($clon);
-//   });
-
-//   $cards.appendChild($fragment);
-// }
-
-console.log(document.location.pathname);
-
 let url = document.location.pathname;
 
-if (url.includes("male")) {
+if (url.includes("hombre")) {
   generarCard("hombre");
 }
 
-if (url.includes("female")) {
+if (url.includes("mujer")) {
   generarCard("mujer");
 }
 
 if (url.includes("show")) {
-  console.log("Lol");
+  generarShow();
 }
